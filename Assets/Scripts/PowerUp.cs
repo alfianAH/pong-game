c61269b43054173304e7950fe59b88d0ecb494d3
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-23 17:06:31 +0800

    Add power up object

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -3,10 +3,5 @@ using UnityEngine.UI;
 
 public class PowerUp : MonoBehaviour
 {
-    private Slider slider;
-
-    private void Awake()
-    {
-        slider = GetComponent<Slider>();
-    }
+    
 }
