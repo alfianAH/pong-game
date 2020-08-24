b5f125f43e90470c19bdc40f1c2ef8b5b6f4f26f
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-24 23:47:48 +0800

    Add OpeningScene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// Play game
+    /// </summary>
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Pong");
+    }
+
+    /// <summary>
+    /// Exit game
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
